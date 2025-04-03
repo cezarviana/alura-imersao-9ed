0f70b5460c0b965e8f784010834b227d847d524d
Cezar Viana <cezar.viana@gmail.com>
2025-04-03 16:31:17 -0300

    <mod>: tufOfWar.js and otherway to do glassFloor.js

--- glassFloor.js
+++ glassFloor.js
@@ -18,3 +18,25 @@ while(round <= 3){
 
     round = round + 1
 }
+
+/*
+With "For"
+
+for(let round = 1; round <= 3; round++){
+    console.log("Rodada: "+ round)
+    
+    let playerChoice = prompt("Nível " +round+", escolha o piso: 1, 2 ou 3.")
+    let glassFloor = Math.floor(Math.random() * 3) + 1;
+    
+    //glassFloor quebrou;
+    if(playerChoice == glassFloor){
+        alert("O piso quebrou! Acabou o jogo para você")
+        round = 1000;
+    }else if(round == 3){
+        alert("Parabéns! Você está vivo!")
+    }else{
+        alert("Você passou! Piso quebrado estava na ponte: " +glassFloor+ " Escolha o piso: 1, 2 ou 3.")
+    }
+}
+
+*/
--- tugOfWar.js
+++ tugOfWar.js
@@ -1,16 +1,45 @@
-for(let round = 1; round <= 3; rodada++){
-    console.log("Rodada: "+ round)
-    
-    let playerChoice = prompt("Nível " +round+", escolha o piso: 1, 2 ou 3.")
-    let glassFloor = Math.floor(Math.random() * 3) + 1;
-    
-    //glassFloor quebrou;
-    if(playerChoice == glassFloor){
-        alert("O piso quebrou! Acabou o jogo para você")
-        round = 1000;
-    }else if(round == 0){
-        alert("Parabéns! Você está vivo!")
-    }else{
-        alert("Você passou! Piso quebrado estava na ponte: " +glassFloor+ " Escolha o piso: 1, 2 ou 3.")
-    }
+// criar lista com 3 personagens
+let characters = ["", "", ""]
+
+let vilains = ["", "", ""]
+
+let charactersStrengh = 0
+let vilainsStrengh = 0
+
+alert("O array personagem contém: " + characters)
+
+for(let i=0; i<3; i++){
+    let charactersChoice = prompt("Digite o nome do seu personagem: "+(i+1));
+    characters[i] = charactersChoice
+
+    // Calcular a força de cada personagem, e depois somar pra saber a força do time
+     
+    charactersStrengh += Math.floor(Math.random() * 10) + 1 // === characterStrengh = characterStrengh + Math.floor(Math.random() * 10) + 1
+}
+
+console.log("Agora, o array personagem contém: " + characters)
+
+console.log("Vilões iniciais: " + vilains)
+
+for(let i=0; i<3; i++){
+    randomIndex = Math.floor(Math.random() * 10);
+    randomVilains = ["Coringa", "Darth Vader", "Drácula", "Hannibal Lecter", "Agent Smith", "Sauron", "Palpatine", "Scar", "Hans Gruber", "Hans Landa"]
+    vilains[i] = randomVilains[randomIndex]
+
+    // Calcular a força de cada personagem do time do computador
+
+    vilainsStrengh += Math.floor(Math.random() * 10) + 1
+
+}
+
+console.log("Vilões: "+ vilains)
+
+// Comparar os dois times para saber quem venceu
+
+if(charactersStrengh > vilainsStrengh){
+    alert("Seu time é muito forte! Vocês ganharam! Sua força foi: " + charactersStrengh)
+}else if(charactersStrengh == vilainsStrengh){
+    alert("Seus times possuem forças similares! Vocês empataram")
+}else{
+    alert("Seu time é fraco! Vocês perderam! A força dos vilões foi: " + vilainsStrengh)
 }
